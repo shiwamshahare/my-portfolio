0be44eea3f92f7f98ec0e82ef1ebc23d98d49eac
Shiwam Shahare <com.shahare@gmail.com>
2024-07-11 23:59:41 +0530

    new home svg updaetd

--- src/components/Home/HomeImg.jsx
+++ src/components/Home/HomeImg.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import HomeSvg from "../Svg/HomeSvg/HomeSvg";
 import TiltSvg from "../Svg/TiltSvg";
+import NewHomeSvg from "../Svg/HomeSvg/NewHomeSvg";
 
 const HomeImg = () => {
   return (
     <TiltSvg
       className="md:w-2/5 w-3/5 pt-20 sm:pt-12 ease-in-out duration-75"
-      SvgContet={<HomeSvg />}
+      SvgContet={
+        <NewHomeSvg />
+        // <HomeSvg />
+      }
     />
   );
 };
