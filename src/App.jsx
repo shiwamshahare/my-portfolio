920fa65fd0c7d31ae1b62a7428f23905c9fe0d1c
Shiwam Shahare <com.shahare@gmail.com>
2024-08-09 11:55:49 +0530

    added locomotive for scroll smoothness

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import "./App.css";
 import { Outlet } from "react-router-dom";
-
+import LocomotiveScroll from "locomotive-scroll";
 function App() {
+  const locomotiveScroll = new LocomotiveScroll();
   return (
     <>
       <Header />
